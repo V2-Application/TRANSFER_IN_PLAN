8e7904ee94d795e00d7de31463269e4018f8e696
V2-Application <147598886+V2-Application@users.noreply.github.com>
2026-04-04 14:24:16 +0530

    Update PurchasePlanController.cs

--- Controllers/PurchasePlanController.cs
+++ Controllers/PurchasePlanController.cs
@@ -108,7 +108,7 @@ namespace TRANSFER_IN_PLAN.Controllers
         private static string Q(string? s)
         {
             if (string.IsNullOrEmpty(s)) return "";
-            return "\"" + s.Replace("\"", "\"\""") + "\"";
+            return "\"" + s.Replace("\"", "\"\"") + "\"";
         }
     }
 }
